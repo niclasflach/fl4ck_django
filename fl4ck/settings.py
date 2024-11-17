1d08b06357565e5758beae4dcc6b10d241ad1624
niclasflach <fl4cken@gmail.com>
2024-11-17 21:47:48 +0100

    changed allowed hosts

--- fl4ck/settings.py
+++ fl4ck/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5u&i!2i6-ju70$!@-+bf_6vr5dyp&x4r!38cxyomg3l$xboo%m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [fl4ck-django.onrender.com]
 
 
 # Application definition
